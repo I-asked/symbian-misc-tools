3b5b04be3d42f530ff58de655eb16238bcf611cf
Julia Nelz <121945980+I-asked@users.noreply.github.com>
2023-11-03 19:56:30 +0100

    Ugly docker workarounds 😭

--- une32/bin/une32.sh
+++ une32/bin/une32.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-set -e
-#set -x
+set -euo pipefail
 
-INP=${1:?Input filename needed}
+err_report () {
+  echo "==> Error $2 on line $1 of une32.sh"
+}
+trap 'err_report $LINENO $?' ERR
+
+INP="${1:?Input filename needed}"
 shift
-OUT=${1:?Output filename needed}
+OUT="${1:?Output filename needed}"
 shift
 
 readonly DUMMY=$(mktemp)
@@ -19,11 +23,12 @@ SECTIONS=()
 
 trap 'rm -f "${INTERMEDIATES[@]}" "${SECTIONS[@]}"' EXIT
 
-arm-none-symbianelf-gcc -x c "$@" -o "${DUMMY}" - <<EOF
+arm-none-symbianelf-gcc.exe -x c "$@" -o "${DUMMY}" - <<EOF
 int foo = 0;
+const char *bar = "foobar";
 int main(int argc, char **argv) {
-    foo = argc;
-    return foo;
+  foo = argc;
+  return foo;
 }
 EOF
 
@@ -39,11 +44,11 @@ TYPEMAP[.data]+=d
 
 LI="${DUMMY}"
 for k in "${!INTERMEDIATES[@]}"; do
-    LO="${INTERMEDIATES[$k]}"
-    elf2e32 --e32input "${INP}" --dump "${TYPEMAP[$k]}" |grep -P '^[0-9a-f]+:' |xxd -r >"${SECTIONS[$k]}"
-    arm-none-symbianelf-objcopy --update-section .text="${SECTIONS[$k]}" "${LI}" "${LO}"
-    LI="${INTERMEDIATES[$k]}"
+  LO="${INTERMEDIATES[$k]}"
+  /var/lib/nokiaprefix/drive_c/Nokia/devices/Nokia_Symbian_Belle_SDK_v1.0/epoc32/tools/elf2e32.exe --e32input "${INP}" --dump "${TYPEMAP[$k]}" |grep -P '^[0-9a-f]+:' |xxd -r >"${SECTIONS[$k]}" ||:
+  arm-none-symbianelf-objcopy.exe --update-section "$k"="${SECTIONS[$k]}" "${LI}" "${LO}"
+  LI="${INTERMEDIATES[$k]}"
 done
 
 rm -f "${OUT}"
-arm-none-symbianelf-objcopy --only-section=.text,.data "${LO}" "${OUT}"
+arm-none-symbianelf-objcopy.exe --only-section=.text,.data "${LO}" "${OUT}"
